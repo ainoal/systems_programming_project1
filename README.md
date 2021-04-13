1993aeff8fd9a7c6f21cc99631739fbf1c3de155
ainoal <aino.liukkonen@gmail.com>
2021-04-13 17:52:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Käyttöjärjestelmät ja systeemiohjelmointi: project 1
+Warmup to C and Unix programming
+Program to read in the data from an input file and reverse it so that the last line is printed first and the first line is printed last
+
---
+++ reverse.c
@@ -0,0 +1,19 @@
+/*******************************************************************/
+/* CT60A2500 C-ohjelmoinnin perusteet
+ * Otsikkotiedot: L07T2.c
+ * Tekijä: Aino Liukonen
+ * Opiskelijanumero: 05606282 
+ * Päivämäärä: 1.3.2020
+ * Yhteistyö ja lähteet, nimi ja yhteistyön muoto:
+ */
+/*******************************************************************/
+
+#include <stdio.h>
+
+int main(void) { 
+
+	return 0;
+}
+
+/*******************************************************************/
+/* eof */
