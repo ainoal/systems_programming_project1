545c2f22339f5cbd5ff0969b0ba465f5661aa019
ainoal <aino.liukkonen@gmail.com>
2021-05-13 19:48:37 +0300

    error message if same input and output files

--- reverse.c
+++ reverse.c
@@ -17,7 +17,6 @@ int main(int argc, char *argv[]) {
 
 	switch (argc) {
 		case 1:
-
 			pStart = readStdin();
 			reverseLines(&pStart);
 			write(stdout, pStart);
@@ -30,6 +29,10 @@ int main(int argc, char *argv[]) {
 			freeNodes(pStart);
 			break;
 		case 3:
+			if ((strcmp(argv[1], argv[2])) == 0) {
+				fprintf(stderr, "Input and output file must differ\n");
+				exit(1);
+			}
 			pStart = readFile(argv[1]);
 			reverseLines(&pStart);
 			fp = openFile(pStart, argv[2]);
@@ -38,7 +41,7 @@ int main(int argc, char *argv[]) {
 			freeNodes(pStart);
 			break;
 		default:
-			printf("usage: reverse <input> <output>\n");
+			fprintf(stderr, "usage: reverse <input> <output>\n");
 			exit(1);
 	}
 
